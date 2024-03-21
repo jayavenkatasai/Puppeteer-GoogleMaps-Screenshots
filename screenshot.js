28a11768dfc727706b93e8842ed8d3f94f653da3
VENKATA SAI <venkatasaib@smbxl.com>
2024-03-22 02:10:36 +0530

    scheduler-added

--- screenshot.js
+++ screenshot.js
@@ -1,17 +1,28 @@
 const puppeteer = require('puppeteer');
+const cron = require('node-cron');
 
-(async () => {
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
-  
-  await page.setViewport({ width: 1920, height: 1080 }); // Set viewport size
+async function takeScreenshot() {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    
+    await page.setViewport({ width: 1920, height: 1080 }); // Set viewport size
 
-  await page.goto('http://127.0.0.1:5500/googlemaps.html'); // Replace with the path to your HTML file
-  
-  //await page.waitFor(2000); // Wait for some time for the map to load completely
-  await new Promise(resolve => setTimeout(resolve, 20000));
+    await page.goto('https://app.powerbi.com/view?r=eyJrIjoiNzQyYmQ2NWEtMDQ4Yy00NzRlLTkwNmQtYzQwZWJjYWRhZTk1IiwidCI6IjgzYTAwNTRhLTYwY2UtNDYxZi1hMWY2LTBlNzJkMDNjOGRiYiJ9'); // Replace with the correct path to your HTML file
 
-  await page.screenshot({ path: `C:/Users/VENKATA SAI/Desktop/maps/${Date.now()}.png` }); // Replace with the path to your desired folder
+    // Introduce a delay of 2000 milliseconds for the map to load completely
+    await new Promise(resolve => setTimeout(resolve, 20000));
 
-  await browser.close();
-})();
+    const screenshotPath = `C:/Users/VENKATA SAI/Desktop/maps/${Date.now()}.png`;
+
+    await page.screenshot({ path: screenshotPath });
+
+    console.log(`Screenshot saved to ${screenshotPath}`);
+
+    await browser.close();
+}
+
+// Schedule the task to run at 1:30 AM every day
+cron.schedule('57 1 * * *', async () => {
+    console.log('Running scheduled task...');
+    await takeScreenshot();
+});
